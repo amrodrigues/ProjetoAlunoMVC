fb58c41c4e5746fd50f5e04e92a712df6ce3ff9a
annarodrigues2 <anna.rodrigues@minervafoods.com>
2025-06-09 20:28:21 -0300

    Adicionar arquivos de projeto.

---
+++ src/AppFuncional/Controllers/AlunosController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AppFuncional.Data;
+using AppFuncional.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AppFuncional.Controllers
+{
+    [Authorize]
+    [Route("meus-alunos")]
+    public class AlunosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AlunosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Alunos
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Sucesso = "Listagem bem sucedida.";
+            return _context.Aluno != null ? 
+                View(await _context.Aluno.ToListAsync()) : 
+                Problem("Entiy set 'ApplicationDbContext.Aluno' is null.");
+        }
+
+        [Route("detalhes/id:int")]
+        // GET: Alunos/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            if ( _context.Aluno == null)
+            {
+                return NotFound();
+            }
+
+            var aluno = await _context.Aluno
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
+            return View(aluno);
+        }
+
+        [Route("novo")]
+        // GET: Alunos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Alunos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+     
+        [HttpPost("novo")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,DataNascimento,Email,EmailConfirmacao,Avaliacao,Ativo")] Aluno aluno)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(aluno);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aluno);
+        }
+
+        [Route("editar/{id:int}")]
+        // GET: Alunos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var aluno = await _context.Aluno.FindAsync(id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+            return View(aluno);
+        }
+
+        // POST: Alunos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost("editar/{id:int}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,DataNascimento,Email,Avaliacao,Ativo")] Aluno aluno)
+        {
+            if (id != aluno.Id)
+            {
+                return NotFound();
+            }
+            ModelState.Remove("EmailConfirmacao");
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(aluno);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AlunoExists(aluno.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                TempData["Sucesso"] = "Edição bem sucedida.";
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aluno);
+        }
+
+        // GET: Alunos/Delete/5
+        [Route("excluir/{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (_context.Aluno == null)
+            {
+                return NotFound();
+            }
+
+            var aluno = await _context.Aluno
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
+            return View(aluno);
+        }
+
+        // POST: Alunos/Delete/5
+        [HttpPost("excluir/{id:int}"), ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var aluno = await _context.Aluno.FindAsync(id);
+            if (aluno != null)
+            {
+                _context.Aluno.Remove(aluno);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AlunoExists(int id)
+        {
+            return _context.Aluno.Any(e => e.Id == id);
+        }
+    }
+}
